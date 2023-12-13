a391c1ae37037ddf752c0c8e29fdc25210071293
vrajesh0167 <chovatiyavrajesh167@gmail.com>
2023-12-13 16:13:30 +0530

    fix stack spelling mistack in ApiError

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         statusCode,
         message = "Somethig want wrong",
         errors = [],
-        statck = ""
+        stack = ""
     ) {
         super(message);
         this.statusCode = statusCode;
@@ -12,8 +12,8 @@ class ApiError extends Error {
         this.message = message;
         this.errors = errors;
 
-        if (statck) {
-            this.stack = statck;
+        if (stack) {
+            this.stack = stack;
         } else {
             Error.captureStackTrace(this, this.constructor);
         }
